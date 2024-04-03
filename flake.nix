51d5763336c318917c8cf8db36cf74cb07eff0b2
shivaraj-bh <sbh69840@gmail.com>
2024-04-04 01:48:44 +0530

    add cuda and rocm apps; create ollama-stack service

--- flake.nix
+++ flake.nix
@@ -23,6 +23,8 @@
       perSystem = { self', pkgs, config, system, ... }: {
         _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
+          # Required for CUDA
+          config.allowUnfree = true;
           overlays = [
             (self: _: with self; {
               open-webui-frontend = callPackage (import ./nix/open-webui-frontend.nix { inherit inputs; }) { };
@@ -42,65 +44,34 @@
             }
           ];
         };
-        process-compose.default = {
-          imports = [
-            inputs.services-flake.processComposeModules.default
-          ];
-          services.ollama."ollama" = {
-            enable = true;
-            host = "0.0.0.0";
-            models = [ "llama2-uncensored" ];
+        process-compose =
+          let
+            common = { ... }: {
+              imports = [
+                inputs.services-flake.processComposeModules.default
+                (import ./nix/ollama.nix { inherit self' inputs; })
+              ];
+              services.ollama-stack.enable = true;
+            };
+          in
+          {
+            default = {
+              imports = [ common ];
+              services.ollama-stack.open-webui.enable = true;
+            };
+            cuda = {
+              imports = [ common ];
+              services.ollama-stack.extraOllamaConfig = {
+                package = pkgs.ollama.override { acceleration = "cuda"; };
+              };
+            };
+            rocm = {
+              imports = [ common ];
+              services.ollama-stack.extraOllamaConfig = {
+                package = pkgs.ollama.override { acceleration = "rocm"; };
+              };
+            };
           };
-          settings.processes.open-webui =
-            let
-              ollama-cfg = config.process-compose.default.services.ollama.ollama;
-            in
-            {
-              command = pkgs.writeShellApplication {
-                name = "open-webui";
-                runtimeInputs = [ self'.packages.open-webui-backend.pyEnv ];
-                text = ''
-                  set -x
-                  # TODO: make a service and give it a dataDir option
-                  if [ -d ./data/open-webui ]; then
-                    rm -rf ./data/open-webui
-                  fi
-                  mkdir -p ./data/open-webui/build
-                  cp -r ${inputs.open-webui}/. ./data/open-webui
-                  # There is FRONTEND_BUILD_DIR, but backend is hardcoded to find some components of the frontend in the parent directory, like the favicon in `backend/config.py`
-                
-                  cp -r ${pkgs.open-webui-frontend}/share/open-webui/. ./data/open-webui/build
-                  chmod -R u+rw ./data/open-webui
-                  cd ./data/open-webui/backend
-
-                  # TODO: protocol must be an option
-                  OLLAMA_API_BASE_URL=http://${ollama-cfg.host}:${builtins.toString ollama-cfg.port}/api
-                  export OLLAMA_API_BASE_URL
-                  uvicorn main:app --host 0.0.0.0 --port 1111 --forwarded-allow-ips '*'
-                '';
-              };
-              readiness_probe.http_get = {
-                # TODO: wire the host and port config after open-webui is extracted to be a service
-                host = "0.0.0.0";
-                port = 1111;
-              };
-              depends_on."ollama-models".condition = "process_completed_successfully";
-            };
-
-          settings.processes.open-browser =
-            {
-              command = pkgs.writeShellApplication {
-                name = "open-browser";
-                runtimeInputs = if pkgs.stdenv.isLinux then [ pkgs.xdg-utils ] else [ ];
-                # TODO: wire the host and port config after open-webui is extracted to be a service
-                text = ''
-                  ${ if pkgs.stdenv.isLinux then "xdg-open http://0.0.0.0:1111" else "" }
-                  ${ if pkgs.stdenv.isDarwin then "open http://0.0.0.0:1111" else "" }
-                '';
-              };
-              depends_on."open-webui".condition = "process_healthy";
-            };
-        };
       };
     };
 }
---
+++ nix/ollama.nix
@@ -0,0 +1,98 @@
+{ self', inputs }:
+{ pkgs, config, lib, ... }:
+{
+  options = {
+    services.ollama-stack = lib.mkOption {
+      type = lib.types.submodule {
+        options = {
+          enable = lib.mkEnableOption "Enable ollama-stack comprising ollama server and other client processes";
+
+          extraOllamaConfig = lib.mkOption {
+            type = lib.types.deferredModule;
+            default = { };
+            description = ''
+              Extra Ollama service config.
+            '';
+          };
+
+          open-webui = lib.mkOption {
+            type = lib.types.submodule {
+              options = {
+                enable = lib.mkEnableOption "Enable open-webui, an interactive chat web app";
+              };
+            };
+            default = { };
+            defaultText = ''
+              open-webui disabled by default
+            '';
+          };
+        };
+      };
+    };
+  };
+  config =
+    let
+      cfg = config.services.ollama-stack;
+    in
+    lib.mkIf cfg.enable {
+      services.ollama."ollama" = {
+        imports = [ cfg.extraOllamaConfig ];
+        enable = true;
+        host = "0.0.0.0";
+        models = [ "llama2-uncensored" ];
+        extraEnvs = {
+          OLLAMA_DEBUG = "1";
+        };
+      };
+      settings = lib.optionalAttrs (cfg.open-webui.enable) {
+        processes.open-webui =
+          let
+            ollama-cfg = config.services.ollama.ollama;
+          in
+          {
+            command = pkgs.writeShellApplication {
+              name = "open-webui";
+              runtimeInputs = [ self'.packages.open-webui-backend.pyEnv ];
+              text = ''
+                set -x
+                # TODO: make a service and give it a dataDir option
+                if [ -d ./data/open-webui ]; then
+                  rm -rf ./data/open-webui
+                fi
+                mkdir -p ./data/open-webui/build
+                cp -r ${inputs.open-webui}/. ./data/open-webui
+                # There is FRONTEND_BUILD_DIR, but backend is hardcoded to find some components of the frontend in the parent directory, like the favicon in `backend/config.py`
+            
+                cp -r ${pkgs.open-webui-frontend}/share/open-webui/. ./data/open-webui/build
+                chmod -R u+rw ./data/open-webui
+                cd ./data/open-webui/backend
+
+                # TODO: protocol must be an option
+                OLLAMA_API_BASE_URL=http://${ollama-cfg.host}:${builtins.toString ollama-cfg.port}/api
+                export OLLAMA_API_BASE_URL
+                uvicorn main:app --host 0.0.0.0 --port 1111 --forwarded-allow-ips '*'
+              '';
+            };
+            readiness_probe.http_get = {
+              # TODO: wire the host and port config after open-webui is extracted to be a service
+              host = "0.0.0.0";
+              port = 1111;
+            };
+            depends_on."ollama-models".condition = "process_completed_successfully";
+          };
+
+        processes.open-browser = {
+          command = pkgs.writeShellApplication {
+            name = "open-browser";
+            runtimeInputs = if pkgs.stdenv.isLinux then [ pkgs.xdg-utils ] else [ ];
+            # TODO: wire the host and port config after open-webui is extracted to be a service
+            text = ''
+              ${ if pkgs.stdenv.isLinux then "xdg-open http://0.0.0.0:1111" else "" }
+              ${ if pkgs.stdenv.isDarwin then "open http://0.0.0.0:1111" else "" }
+            '';
+          };
+          depends_on."open-webui".condition = "process_healthy";
+        };
+      };
+    };
+}
