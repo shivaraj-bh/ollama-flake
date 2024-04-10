13b0dafca09279a67ab1467176e20b50a5f0a74c
shivaraj-bh <sbh69840@gmail.com>
2024-04-10 13:33:55 +0530

    add FIXME to use the upstream open-webui
    after https://github.com/open-webui/open-webui/pull/1472
    
    after it is released

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     dream2nix.url = "github:nix-community/dream2nix";
     dream2nix.inputs.nixpkgs.follows = "nixpkgs";
+    # FIXME: use upstream after https://github.com/open-webui/open-webui/pull/1472 is released
     open-webui = {
       url = "github:shivaraj-bh/open-webui/304bf9d9b13fa1937a5d5bbff710e4237ca2d62b";
       flake = false;
